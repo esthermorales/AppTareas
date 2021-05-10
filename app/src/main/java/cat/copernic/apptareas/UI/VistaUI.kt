1eb73ae69bb41c693e0ede78ab213bf81aae8476
Emilio <arredondo.sanchez.emilio@alumnat.copernic.cat>
2021-05-10 20:36:50 +0200

    Fuentes def

---
+++ app/src/main/java/cat/copernic/apptareas/UI/VistaUI.kt
@@ -0,0 +1,12 @@
+package cat.copernic.apptareas.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.copernic.apptareas.R
+
+class VistaUI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_ui)
+    }
+}
