f2cee3cbf589658ac4df1c4f631023ece3052bc6
joseceuve <71776803+joseceuve@users.noreply.github.com>
2021-05-31 17:52:26 +0200

    Añadido compartir

--- app/src/main/java/cat/copernic/apptareas/Datos/DBCompartido.kt
+++ app/src/main/java/cat/copernic/apptareas/Datos/DBCompartido.kt
@@ -14,54 +14,69 @@ class DBCompartido {
      * Guarda la instancia de los datos compartidos que se incluye en lista tareas
      *
      */
-    fun insertar(lista: ListaTareas){
+    fun insertar(lista: ListaTareas) {
         //Si esta compartido con alguien
-        if(lista.compartido != null){
+        if (lista.compartido != null) {
             lista.compartido!!.forEach { n ->
-                Log.e("Test",n.email)
+                Log.e("Test", n.email)
                 //Por cada elemento se añade a la db especifica y genera el id auto
-                db.collection("compartido").document(n.email.toString()+"-"+lista.idLista)
-                    .set(hashMapOf("email" to n.email.toString(), "listaTareas" to lista.idLista.toString()))
-            }
-        }
-    }
-
-    fun recuperar(lista: ArrayList<ListaTareas>, dostuff: (listaa: ArrayList<String>) -> Unit){
-        var listaEmailString =  ArrayList<String>()
-        coleccion.get().addOnSuccessListener {
-            for (document in it) {
-                for (lis in lista){
-                    var numId: String = document.get("listaTareas") as String
-                    if (numId.toInt() == lis.idLista){
-                        //si el documento es igual a la lista
-                        listaEmailString.add(document.get("email") as String)
-                    }
-                    ejecutarCodigo(lis.idLista, listaEmailString)
-                    //Para ejecutar de manera remota, recibe un arrayList con el mail
-                    dostuff(listaEmailString)
-                    listaEmailString.clear()
-                }
-
+                db.collection("compartido").document(n.email.toString() + "-" + lista.idLista)
+                    .set(
+                        hashMapOf(
+                            "email" to n.email.toString(),
+                            "listaTareas" to lista.idLista.toString()
+                        )
+                    )
             }
         }
     }
 
     /**
-     * Codigo a ejecutar
+     * Para añadir un unico usuario
      */
-    fun ejecutarCodigo(idLista: Int, emailsUsuarios: ArrayList<String>){
-        mostrar(idLista, emailsUsuarios)
-
-
+    fun insertarMinimalista(idLista: Int, usuario: String) {
+        db.collection("compartido").document(usuario + "-" + idLista.toString())
+            .set(hashMapOf("email" to usuario, "listaTareas" to idLista.toString()))
     }
 
-    //Ejemplo
-    fun mostrar(int: Int,texto: ArrayList<String>){
-        println("Compartido en " + int)
-        for (tx in texto)
-            println("-> " + texto)
 
+
+fun recuperar(lista: ArrayList<ListaTareas>, dostuff: (listaa: ArrayList<String>) -> Unit) {
+    var listaEmailString = ArrayList<String>()
+    coleccion.get().addOnSuccessListener {
+        for (document in it) {
+            for (lis in lista) {
+                var numId: String = document.get("listaTareas") as String
+                if (numId.toInt() == lis.idLista) {
+                    //si el documento es igual a la lista
+                    listaEmailString.add(document.get("email") as String)
+                }
+                ejecutarCodigo(lis.idLista, listaEmailString)
+                //Para ejecutar de manera remota, recibe un arrayList con el mail
+                dostuff(listaEmailString)
+                listaEmailString.clear()
+            }
+
+        }
     }
+}
+
+/**
+ * Codigo a ejecutar
+ */
+fun ejecutarCodigo(idLista: Int, emailsUsuarios: ArrayList<String>) {
+    mostrar(idLista, emailsUsuarios)
+
+
+}
+
+//Ejemplo
+fun mostrar(int: Int, texto: ArrayList<String>) {
+    println("Compartido en " + int)
+    for (tx in texto)
+        println("-> " + texto)
+
+}
 
 
 
--- app/src/main/java/cat/copernic/apptareas/UI/FragmentCompartirLista.kt
+++ app/src/main/java/cat/copernic/apptareas/UI/FragmentCompartirLista.kt
@@ -1,23 +1,34 @@
 package cat.copernic.apptareas.UI
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import cat.copernic.apptareas.Datos.DBCompartido
 import cat.copernic.apptareas.Modelos.ListaTareas
 import cat.copernic.apptareas.Modelos.Usuario
 import cat.copernic.apptareas.R
+import cat.copernic.apptareas.databinding.FragmentCompartirListaBinding
+import cat.copernic.apptareas.databinding.FragmentTareasBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import org.jetbrains.annotations.Nls
 
 class FragmentCompartirLista : Fragment() {
-    var tareas = ArrayList<ListaTareas>()
-    var emails = ArrayList<String>()
+    var idDeLaLista = 1
+    private lateinit var binding: FragmentCompartirListaBinding
+            private val db = FirebaseFirestore.getInstance()
+    private val coleccion = db.collection("compartido")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("Jose","Estoy aqui")
         val dbcompartido = DBCompartido()
-        dbcompartido.recuperar(tareas, ::recuperarCopartido)
+
+        Log.i("Jose","Ahora aqui")
 
     }
 
@@ -25,20 +36,48 @@ class FragmentCompartirLista : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compartir_lista, container, false)
-
-
-
-    }
-
-
-
-    fun recuperarCopartido(lista: ArrayList<String>){
-        for (mail in lista){
-            emails.add(mail)
-            println("--->"+mail)
+        // Inflate the layout for this frag
+        binding = FragmentCompartirListaBinding.inflate(inflater, container, false)
+        val dbcompartido = DBCompartido()
+        //Al hacer clic al añadir usuario
+        binding.addUser.setOnClickListener {
+            //Si tiene contenido
+            if (!binding.editTextTextEmailAddress.text.equals("")){
+                //Inserta al usuario en la db
+                dbcompartido.insertarMinimalista(idDeLaLista,binding.editTextTextEmailAddress.text.toString())
+                //Limpia el txt
+                var limpia = ""
+                binding.editTextTextEmailAddress.text = null
+            }
         }
+
+
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+        Log.i("Jose","Estoy en este punto")
+        coleccion.whereEqualTo("listaTareas", idDeLaLista.toString()).get().addOnSuccessListener {
+            for (document in it) {
+                if (it != null){
+                    Log.i("Jose",document.data.get("email") as String)
+                    if (binding.multilineaUsuers.text.equals("")){
+                        binding.multilineaUsuers.setText(document.data.get("email") as String)
+                    }else{
+                        binding.multilineaUsuers.setText( binding.multilineaUsuers.text.toString()
+                                + "\n" + document.data.get("email") as String)
+                    }
+
+                }
+            }
+        }
+
+    }
+
 
 }
