860a7e38e4a993c93fab7b1481636c93689b1dd3
morales.mozas.esther <morales.mozas.esther@alumnat.copernic.cat>
2021-05-31 18:03:08 +0200

    commit

--- app/src/main/java/cat/copernic/apptareas/UI/ViewListas/ListaTareasAdapter.kt
+++ app/src/main/java/cat/copernic/apptareas/UI/ViewListas/ListaTareasAdapter.kt
@@ -7,11 +7,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.apptareas.Modelos.ListaTareas
 import cat.copernic.apptareas.R
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_item_listas.view.*
 
 class ListaTareasAdapter(private val clickListener: ListaTareasAdapter.OnUserClic):
     RecyclerView.Adapter<ListaTareasAdapter.ListaTareasViewHolder>() {
 
     private var dataList = mutableListOf<ListaTareas>()
+     //lateinit var mFirebase: FirebaseFirestore
+     private val db = FirebaseFirestore.getInstance()
 
 
     fun setListData(data: MutableList<ListaTareas>) {
@@ -29,6 +34,12 @@ class ListaTareasAdapter(private val clickListener: ListaTareasAdapter.OnUserCli
 
             categoria.text = element.categoria
             itemView.setOnClickListener { clickListener.onUserClickAction(element) }
+
+            itemView.idDelete.setOnClickListener{
+              //  mFirebase = FirebaseFirestore.getInstance()
+                db.collection("listaTareas").document(element.idLista.toString()).delete()
+
+            }
         }
     }
 
@@ -39,8 +50,12 @@ class ListaTareasAdapter(private val clickListener: ListaTareasAdapter.OnUserCli
     }
 
     override fun onBindViewHolder(holder: ListaTareasViewHolder, position: Int) {
+
+        //val listaDocumento: DocumentSnapshot = getSnapshots().getSnapshot(holder.adapterPosition)
+
         val user = dataList[position]
         holder.bindView(user)
+
     }
 
     override fun getItemCount(): Int {
