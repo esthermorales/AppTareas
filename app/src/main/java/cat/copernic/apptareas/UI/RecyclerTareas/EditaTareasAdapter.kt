d548d116d2408277485a3f4b7b12e0207c252696
Emilio <arredondo.sanchez.emilio@alumnat.copernic.cat>
2021-06-01 18:43:59 +0200

    Edit tarea

--- app/src/main/java/cat/copernic/apptareas/UI/RecyclerTareas/EditaTareasAdapter.kt
+++ app/src/main/java/cat/copernic/apptareas/UI/RecyclerTareas/EditaTareasAdapter.kt
@@ -4,27 +4,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.apptareas.Modelos.ElementoTarea
 import cat.copernic.apptareas.R
 
-class EditaTareasAdapter():
+class EditaTareasAdapter(private val clickListener: OnTareaClic):
     RecyclerView.Adapter<EditaTareasAdapter.EditaTareasViewHolder>() {
 
     private var dataList = ArrayList<ElementoTarea>()
 
-
     fun setListData(data: ArrayList<ElementoTarea>) {
         dataList = data
+    }
+
+    interface OnTareaClic {
+        fun onButtonUpClic(elemento: ElementoTarea)
+        fun onButtonDownClic(elemento: ElementoTarea)
+        fun onButtonDelClic(elemento: ElementoTarea)
     }
 
     inner class EditaTareasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindView(element: ElementoTarea) {
             val nombre: TextView = itemView.findViewById(R.id.txtEditElemento)
+            val botonUp: ImageButton = itemView.findViewById(R.id.imageButton3)
+            val botondown: ImageButton = itemView.findViewById(R.id.btnBajarElemento)
+            val eliminar: ImageButton = itemView.findViewById(R.id.btnDeleteElemento)
 
             nombre.text = element.tarea
+
+            botonUp.setOnClickListener { clickListener.onButtonUpClic(element) }
+            botondown.setOnClickListener { clickListener.onButtonDownClic(element) }
+            eliminar.setOnClickListener { clickListener.onButtonDelClic(element) }
+
         }
     }
 
--- app/src/main/java/cat/copernic/apptareas/UI/tareas.kt
+++ app/src/main/java/cat/copernic/apptareas/UI/tareas.kt
@@ -25,7 +25,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Source
 import kotlinx.android.synthetic.main.pop_up_crear_tarea.view.*
 
-class tareas : Fragment(), TareasAdapter.OnTareaClic {
+class tareas : Fragment(), TareasAdapter.OnTareaClic, EditaTareasAdapter.OnTareaClic {
 
     private lateinit var binding: FragmentTareasBinding
     private lateinit var popu: PopUpCrearTareaBinding
@@ -39,6 +39,7 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
     private lateinit var tarea: ElementoTarea
     private lateinit var adapter: TareasAdapter
     private lateinit var editAdapter: EditaTareasAdapter
+    var elementosGenerales = ArrayList<ElementoTarea>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,15 +48,9 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
         // Inflate the layout for this fragment
         binding = FragmentTareasBinding.inflate(inflater, container, false)
 
-        val identificador = args.listaID
-
         setHasOptionsMenu(true)
 
-        adapter = TareasAdapter(this)
-        binding.LlistaFaqsView.layoutManager = LinearLayoutManager(context)
-        binding.LlistaFaqsView.adapter = adapter
-
-        dbElemento.recuperar(identificador.toString(), ::recuperaElementos)
+        iniciaRecyclerView()
 
         return binding.root
     }
@@ -75,7 +70,7 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
                         (document.data.get("idLista") as String).toInt(),
                         document.data.get("nombre") as String, document.data.get("categoria") as String
                     )
-                    println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + listaTareasTmp.nombre)
+
                     lista = listaTareasTmp
 
                     vista(listaTareasTmp)
@@ -85,11 +80,19 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
         }
     }
 
+
+    fun iniciaRecyclerView() {
+        adapter = TareasAdapter(this)
+        binding.LlistaFaqsView.layoutManager = LinearLayoutManager(context)
+        binding.LlistaFaqsView.adapter = adapter
+
+        dbElemento.recuperar(args.listaID.toString(), ::recuperaElementos)
+    }
+
     fun vista(list: ListaTareas){
 
         binding.idTvTitulo.text = list.categoria
 
-        println("**>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + dbLstas.ultimoNumero)
         binding.addLista.setOnClickListener {
             popPupEmergente()
         }
@@ -131,15 +134,19 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
     }
 
     fun recuperaElementos(list : ArrayList<ElementoTarea>){
+        list.sort()
+        elementosGenerales = list
         adapter.setListData(list)
         adapter.notifyDataSetChanged()
 
     }
 
-    override fun onUserClickAction(elemento: ElementoTarea) {
-        elemento.hecho = !elemento.hecho
-        dbElemento.insertar(elemento)
-        adapter.notifyDataSetChanged()
+    fun recuperaElementosEdit(list : ArrayList<ElementoTarea>){
+        list.sort()
+        elementosGenerales = list
+        editAdapter.setListData(list)
+        editAdapter.notifyDataSetChanged()
+
     }
 
     override fun onPrepareOptionsMenu(menu: Menu) {
@@ -148,6 +155,49 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
         val hola = menu.findItem(R.id.gestionaTareas)
         setings.isVisible = false
         hola.isVisible = true
+    }
+
+    fun subirElemento(posicion: Int) : Boolean {
+        //Si hay elementos, la posicion es mayor de 0 (que no este en el prier cuadro)
+        //y que lal posicion no sea la ultima casilla
+        if (elementosGenerales.size > 0 && posicion > 0 && posicion < elementosGenerales.size) {
+            cambiarPosicion(elementosGenerales.get(posicion), elementosGenerales.get(posicion - 1))
+            return true
+        }else
+            return false
+    }
+
+    fun bajarElemento(posicion: Int) : Boolean {
+        //Si hay elementos, la posicion es mayor de 0 (que no este en el prier cuadro)
+        //y que lal posicion no sea la ultima casilla
+        if (elementosGenerales.size > 0 && posicion >= 0 && posicion < elementosGenerales.size - 1) {
+            cambiarPosicion(elementosGenerales.get(posicion), elementosGenerales.get(posicion + 1))
+            return true
+        }else
+            return false
+    }
+
+
+
+    // Metodos Heredados
+
+
+    /**
+     * cambia la posicion de dos elementos
+     */
+    fun cambiarPosicion(primero: ElementoTarea, segundo: ElementoTarea) {
+        var tmp: Int = 0
+        if (primero != null && segundo != null) {
+            tmp = primero.posicion
+            primero.posicion = segundo.posicion
+            segundo.posicion = tmp
+        }
+    }
+
+    override fun onUserClickAction(elemento: ElementoTarea) {
+        elemento.hecho = !elemento.hecho
+        dbElemento.insertar(elemento)
+        adapter.notifyDataSetChanged()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -161,7 +211,7 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
                 true
             }
             R.id.gestionaTareas ->{
-                editAdapter = EditaTareasAdapter()
+                editAdapter = EditaTareasAdapter(this)
                 binding.LlistaFaqsView.layoutManager = LinearLayoutManager(context)
                 binding.LlistaFaqsView.adapter = editAdapter
 
@@ -172,10 +222,34 @@ class tareas : Fragment(), TareasAdapter.OnTareaClic {
         }
     }
 
-    fun recuperaElementosEdit(list : ArrayList<ElementoTarea>){
-        editAdapter.setListData(list)
-        editAdapter.notifyDataSetChanged()
+    override fun onButtonUpClic(elemento: ElementoTarea) {
+        val indice = elementosGenerales.indexOf(elemento)
+        val continua = subirElemento(indice)
 
+        if (continua){
+            dbElemento.insertar(elementosGenerales.get(indice))
+            dbElemento.insertar(elementosGenerales.get(indice - 1))
+
+            dbElemento.recuperar(args.listaID.toString(), ::recuperaElementosEdit)
+        }
+    }
+
+    override fun onButtonDownClic(elemento: ElementoTarea) {
+        val indice = elementosGenerales.indexOf(elemento)
+        val continua = bajarElemento(indice)
+
+        if (continua){
+            dbElemento.insertar(elementosGenerales.get(indice))
+            dbElemento.insertar(elementosGenerales.get(indice + 1))
+
+            dbElemento.recuperar(args.listaID.toString(), ::recuperaElementosEdit)
+        }
+    }
+
+    override fun onButtonDelClic(elemento: ElementoTarea) {
+        println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + elemento.idElemento)
+        db.collection("elemento").document(elemento.idElemento.toString()).delete()
+        dbElemento.recuperar(args.listaID.toString(), ::recuperaElementosEdit)
     }
 
 }
